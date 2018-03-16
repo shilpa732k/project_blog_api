432de5ead872cac18bf3dc929d1e86f770b55c28
L <vishwas.l@capgemini.com>
2018-03-16 16:11:04 +0530

    session

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express')
 var router = express.Router()
 var User = require('../lib/User')
 
+
+
 router.get('/',function(req,res,next) {
-    res.render('index', {title: 'Express'})
+    res.render('index')
 })
 
 
@@ -20,10 +22,18 @@ router.post('/login', function(req,res) {
         if(!user) {
             return res.status(404).send()
         }
+        req.session.user = user
         return res.status(200).send()
     })
             
             })
+
+router.get('/dashboard', function(req, res) {
+    if(!req.session.user) {
+       return res.status(401).send()
+       }
+    return res.status(200).send("welcome to session login")
+})
 router.post('/register',function(req,res) {
     var username = req.body.username
     var password = req.body.password
@@ -54,5 +64,13 @@ router.get('/listUser',function(req,res) {
         res.json(users);
     });
 })
+
+router.delete('/listuser/:id',function(req, res) {
+    User.findByIdAndRemove(req.params.id).exec().then(doc =>{
+        if(!doc) {return res.status(404).end()}
+        return res.status(204).end()
+    })
+    .catch(err => next(err))
+}) 
 
 module.exports = router
